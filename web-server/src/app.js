44d050651e703dcad1a6e862a378f75e59215e12
jennifer-manriquez <jennymanb@gmail.com>
2026-07-13 23:26:43 -0600

    use express static publicDirectoryPath

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,25 @@
+const path = require("path");
 const express = require("express");
 // express is a function
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// You can now access the help and about pages by going to localhost:3000/help.html and localhost:3000/about.html
+
+app.use(express.static(publicDirectoryPath));
+
+// This will not run as we declared above the static directory. It will only show the first match, so this will be ignored.
 app.get("", (req, res) => {
-  res.send("Hello Express!");
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send("<h1>Weather</h1>!");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Your weather");
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
 });
 
 app.listen(3000, () => {
